9e0ae220a05b23bea7f75ebd9b5d0b945500d1b1
Alina liu <119249419+Abnormalina@users.noreply.github.com>
2024-04-20 19:46:08 -0700

    Update APLine.java

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -1,5 +1,18 @@
 public class APLine
 {
-  /* your code here */
-  
+  private int a, b, c;
+  public APLine(int a, int b, int c){
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  }
+  public double getSlope(){
+    return -(this.a)/(double)(this.b);
+  }
+  public boolean isOnline(int x, int y){
+    if(((this.a)*x + (this.b)*y + this.c)==0){
+      return true;
+    }
+    return false;
+  }
 }
