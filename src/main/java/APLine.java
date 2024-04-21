7cac473a420aeb966102edb19bcfcad9d726f8e5
Alina liu <119249419+Abnormalina@users.noreply.github.com>
2024-04-20 19:56:34 -0700

    Update APLine.java

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -10,5 +10,8 @@ public class APLine
     return -(double)a1/b1;
   }
   public boolean isOnline(int x, int y){
-    return ((a1*x + b1*y + c1)==0);
+    if ((a1*x + b1*y + c1)==0){
+      return true;
+    }
+    return false; }
 }
